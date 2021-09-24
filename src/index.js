981298f05f06139ac3465d452d83a04b6167cfd9
Osama Khan <Osama6756@gmail.com>
2021-09-24 13:12:14 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
